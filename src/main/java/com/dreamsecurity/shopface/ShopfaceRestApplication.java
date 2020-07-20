f7766dcc91d3b555d014fa561a616d37af907916
gmu1026 <gmu01026@gmail.com>
2020-07-20 15:47:41 +0900

    initial commit

---
+++ src/main/java/com/dreamsecurity/shopface/ShopfaceRestApplication.java
@@ -0,0 +1,13 @@
+package com.dreamsecurity.shopface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopfaceRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopfaceRestApplication.class, args);
+	}
+
+}
