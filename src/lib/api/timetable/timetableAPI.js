fdde1f1cefef759a04e87e3694f4e54d787fedf2
min950903 <min950903@gmail.com>
2020-08-11 17:32:01 +0900

    API URL 수정

--- src/lib/api/timetable/timetableAPI.js
+++ src/lib/api/timetable/timetableAPI.js
@@ -1,11 +1,11 @@
 import client from '../client';
 
 export const getTimetableList = async ({ selectecBranch }) => {
-  const response = await client.get('https://iamchan.net/timetable');
+  const response = await client.get('/timetable');
   return { data: response.data };
 };
 
 export const getTimetable = async ({ no }) => {
-  const response = await client.get('https://iamchan.net/timetable');
+  const response = await client.get('/timetable');
   return { data: response.data };
 };
