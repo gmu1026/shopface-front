0198bcd0978dd847180fc9b60ac0b3c459762082
min950903 <min950903@gmail.com>
2020-08-13 15:01:17 +0900

    기존 근무자 지점 추가 오류 수정

--- src/lib/api/member/memberAPI.js
+++ src/lib/api/member/memberAPI.js
@@ -2,11 +2,11 @@ import client from '../client';
 
 export const getMemberList = async () => {
   const response = await client.get('/member');
-  return response.data;
+  return response;
 };
 export const getMember = async ({ id }) => {
   const response = await client.get(`/member/${id}`);
-  return response.data;
+  return response;
 };
 
 export const postMember = ({ data }) => {
@@ -16,10 +16,10 @@ export const postMember = ({ data }) => {
 
 export const putMember = async ({ id, data }) => {
   const response = await client.put(`/member/${id}`, data);
-  return response.data;
+  return response;
 };
 
 export const deleteMember = async ({ id }) => {
   const response = await client.delete(`/member/${id}`);
-  return response.data;
+  return response;
 };
--- src/modules/common/certCode.js
+++ src/modules/common/certCode.js
@@ -52,7 +52,7 @@ export function* certCodeSaga() {
 }
 
 const initialState = {
-  certCode: '',
+  certCode: null,
   certCodeResult: null,
   certCodeError: null,
 };
