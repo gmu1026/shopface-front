93bd65758632c90591453a5d1134ce5c5f57e9c2
min950903 <min950903@gmail.com>
2020-08-26 09:48:39 +0900

    edit dashBoardPage form

--- src/components/dashboard/EmployDashboard.js
+++ src/components/dashboard/EmployDashboard.js
@@ -98,114 +98,102 @@ const EmployDashboard = ({ employR, employW, employC, onWork, onQuit }) => {
         <div className="col-12">
           <div className="card">
             <div className="card-body">
-              <div className="dataTables_wrapper dt-bootstrap4 no-footer">
-                <div className="row">
-                  <div className="col-sm-12 col-md-6">
-                    <div className="form-group" />
-                  </div>
-                  <div className="col-sm-12 col-md-6">
-                    <div className="row">
-                      <div className="form-inline col-5"></div>
-                      <div className="form-inline col ml-5"></div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-sm-12">
-                <h5>현재 스케줄</h5>
-                <table
-                  className="table table-striped dataTable no-footer dtr-inline"
-                  role="grid"
-                  aria-describedby="datatables-buttons_info"
-                >
-                  <tbody>
-                    {employR !== null && employR.length > 0 ? (
-                      employR.map((Rtable, index) => (
-                        <RTableBody
-                          key={index}
-                          Rtable={Rtable}
-                          onWork={onWork}
-                          onQuit={onQuit}
-                        ></RTableBody>
-                      ))
-                    ) : (
-                      <>
-                        <tr role="row">
-                          <td colSpan="4">데이터가 없습니다.</td>
-                        </tr>
-                      </>
-                    )}
-                  </tbody>
-                </table>
+              <div className="dataTables_wrapper dt-bootstrap4 no-footer"></div>
+              <div className="row">
+                <div className="col-sm-12">
+                  <h5>현재 스케줄</h5>
+                  <table
+                    className="table table-striped dataTable no-footer dtr-inline"
+                    role="grid"
+                    aria-describedby="datatables-buttons_info"
+                  >
+                    <tbody>
+                      {employR !== null && employR.length > 0 ? (
+                        employR.map((Rtable, index) => (
+                          <RTableBody
+                            key={index}
+                            Rtable={Rtable}
+                            onWork={onWork}
+                            onQuit={onQuit}
+                          ></RTableBody>
+                        ))
+                      ) : (
+                        <>
+                          <tr role="row">
+                            <td colSpan="4">데이터가 없습니다.</td>
+                          </tr>
+                        </>
+                      )}
+                    </tbody>
+                  </table>
 
-                <h5>예정스케줄</h5>
-                <table
-                  className="table table-striped dataTable no-footer dtr-inline"
-                  role="grid"
-                  aria-describedby="datatables-buttons_info"
-                >
-                  <thead>
-                    <tr role="row">
-                      <th>근무지</th>
-                      <th>담당 업무</th>
-                      <th>스케줄</th>
-                      <th>예상 시간</th>
-                      <th>예상급여</th>
-                      <th>상태</th>
-                      <th>실제시간</th>
-                      <th>실제 급여</th>
-                      <th></th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {employW !== null && employW.length > 0 ? (
-                      employW.map((Wtable, index) => (
-                        <WTableBody key={index} Wtable={Wtable}></WTableBody>
-                      ))
-                    ) : (
-                      <>
-                        <tr role="row">
-                          <td colSpan="4">데이터가 없습니다.</td>
-                        </tr>
-                      </>
-                    )}
-                  </tbody>
-                </table>
-                <h5>지난스케줄</h5>
-                <table
-                  className="table table-striped dataTable no-footer dtr-inline"
-                  role="grid"
-                  aria-describedby="datatables-buttons_info"
-                >
-                  <thead>
-                    <tr role="row">
-                      <th>근무지</th>
-                      <th>담당 업무</th>
-                      <th>스케줄</th>
-                      <th>예상 시간</th>
-                      <th>예상급여</th>
-                      <th>상태</th>
-                      <th>실제시간</th>
-                      <th>실제 급여</th>
-                      <th></th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {employC !== null && employC.length > 0 ? (
-                      employC.map((Ctable, index) => (
-                        <CTableBody key={index} Ctable={Ctable}></CTableBody>
-                      ))
-                    ) : (
-                      <>
-                        <tr role="row">
-                          <td colSpan="4">데이터가 없습니다.</td>
-                        </tr>
-                      </>
-                    )}
-                  </tbody>
-                </table>
+                  <h5>예정스케줄</h5>
+                  <table
+                    className="table table-striped dataTable no-footer dtr-inline"
+                    role="grid"
+                    aria-describedby="datatables-buttons_info"
+                  >
+                    <thead>
+                      <tr role="row">
+                        <th>근무지</th>
+                        <th>담당 업무</th>
+                        <th>스케줄</th>
+                        <th>예상 시간</th>
+                        <th>예상급여</th>
+                        <th>상태</th>
+                        <th>실제시간</th>
+                        <th>실제 급여</th>
+                        <th></th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {employW !== null && employW.length > 0 ? (
+                        employW.map((Wtable, index) => (
+                          <WTableBody key={index} Wtable={Wtable}></WTableBody>
+                        ))
+                      ) : (
+                        <>
+                          <tr role="row">
+                            <td colSpan="4">데이터가 없습니다.</td>
+                          </tr>
+                        </>
+                      )}
+                    </tbody>
+                  </table>
+                  <h5>지난스케줄</h5>
+                  <table
+                    className="table table-striped dataTable no-footer dtr-inline"
+                    role="grid"
+                    aria-describedby="datatables-buttons_info"
+                  >
+                    <thead>
+                      <tr role="row">
+                        <th>근무지</th>
+                        <th>담당 업무</th>
+                        <th>스케줄</th>
+                        <th>예상 시간</th>
+                        <th>예상급여</th>
+                        <th>상태</th>
+                        <th>실제시간</th>
+                        <th>실제 급여</th>
+                        <th></th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {employC !== null && employC.length > 0 ? (
+                        employC.map((Ctable, index) => (
+                          <CTableBody key={index} Ctable={Ctable}></CTableBody>
+                        ))
+                      ) : (
+                        <>
+                          <tr role="row">
+                            <td colSpan="4">데이터가 없습니다.</td>
+                          </tr>
+                        </>
+                      )}
+                    </tbody>
+                  </table>
+                </div>
               </div>
             </div>
           </div>
